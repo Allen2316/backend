26006ddc1de42bac3626f8b4f006780002808344
Allen2316 <luissar14@gmail.com>
2022-07-05 00:14:35 -0500

    Finalizacion de proyecto php

--- expenses/config/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-define("URL", "http://localhost:80/expenses");
-
-define("HOST", "localhost");
-define("DB", "expense-app");
-define("USER", "root");
-define("PASSWORD", "1234");
-define("CHARSET", "utf8mb4");
